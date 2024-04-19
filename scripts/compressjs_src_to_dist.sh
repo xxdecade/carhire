b82bedf3f863011740fc53e5d4a3882fa50d38cf
jyh <xxdecade@163.com>
2024-04-19 11:14:01 +0800

    repair html

--- scripts/compressjs_src_to_dist.sh
+++ scripts/compressjs_src_to_dist.sh
@@ -4,5 +4,5 @@ JS_PATH=/home/jyh/carhire/hire/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}hire.js
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}all.js
 
