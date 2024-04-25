aced0c3d021e726fcbf1b6643bdf3d2a3a48a2f5
jyh <xxdecade@163.com>
2024-04-25 12:42:26 +0800

    finish avatar

--- hire/static/js/src/home/zbase.js
+++ hire/static/js/src/home/zbase.js
@@ -28,25 +28,41 @@ class HireHome {
                             </li>
                         </ul>
 
+                        <div class="user_info_dropdown">
+                            <button class="user_info_dropdown_button">
+                                <img class="user_avatar" src="https://cdn.acwing.com/media/user/profile/photo/256625_sm_a8f857996e.jpg">
+                            </button>
+                            <ul class="user_info_dropdown_menu">
+                                <li class="logout">
+                                    <button>退出登录</button>
+                                </li>
+                            </ul>
+                        </div>
+
                         <div class="header_contact">
                             <div class="telephone">
                                 400-616-6666
                             </div>
                         </div>
-
-                        <div class="logout">
-                            <button>退出登录</button>
+                            
                         </div>
                     </div>
                 </div>
 
                 <div id="main_page" class="content">
+                    <a>首页</a>
                 </div>
 
-                <div id = "hire_detial" class="content">
+                <div id="hire_detial" class="content">
+                    <a>租车详情</a>
                 </div>
 
-                <div id = "hire_map "class="content">
+                <div id="hire_map "class="content">
+                    <a>网点查询</a>
+                </div>
+
+                <div id="activity" class="content">
+                    <a>精彩活动</a>
                 </div>
 
             </div>
@@ -58,6 +74,12 @@ class HireHome {
         this.$mainpage = this.$home.find('div[id="main_page"]');
         this.$hire_detial = this.$home.find('div[id="hire_detial"]');
         this.$hire_map = this.$home.find('div[id="hire_map"]');
+        this.$activity = this.$home.find('div[id="activity"]');
+
+        this.$dropdown_menu = this.$home.find('ul[class="user_info_dropdown_menu"]');
+        this.$dropdown_menu.hide();
+
+        this.$dropdown_button = this.$home.find('button[class="user_info_dropdown_button"]');
 
         this.$logout = this.$home.find('.logout button');
     
@@ -65,6 +87,9 @@ class HireHome {
     }
 
     start() {
+        this.$hire_detial.hide();
+        this.$hire_map.hide();
+        this.$activity.hide();
         this.add_listening_events();
     }
 
@@ -72,6 +97,27 @@ class HireHome {
         let outer = this;
         this.$logout.on('click', function() {
             outer.root.settings.logout_on_remote();
+        });
+
+        this.add_event_show_dropdown_menu();
+    }
+
+    add_event_logout() {
+        let outer = this;
+        this.$logout.on('click', function() {
+            outer.root.settings.logout_on_remote();
+        });
+    }
+
+    add_event_show_dropdown_menu() {
+        let outer = this;
+
+        this.$dropdown_button.on('click', function() {
+            if (outer.$dropdown_menu.is(':hidden')) {
+                outer.$dropdown_menu.show();
+            } else {
+                outer.$dropdown_menu.hide();
+            }
         });
     }
 
