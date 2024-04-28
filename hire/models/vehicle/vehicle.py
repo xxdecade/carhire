63f4ff2f5c668a6242c98af6865205252237c2df
jyh <xxdecade@163.com>
2024-04-28 16:43:35 +0800

    add vehicle model

--- hire/models/vehicle/vehicle.py
+++ hire/models/vehicle/vehicle.py
@@ -4,11 +4,12 @@ from django.utils import timezone
 class Vehicle(models.Model):
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    is_hired = models.BooleanField(default=False)  # 使用下划线来保持 Python 的命名习惯
-    hired_client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True, blank=True)  # 使用外键关联到 Client 模型
+    category = models.CharField(max_length=100, choices=[('SUV', 'SUV'), ('Sedan', 'Sedan'), ('Truck', 'Truck'), ('Van', 'Van')])
+    is_hired = models.BooleanField(default=False)  
+    hired_client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True, blank=True) 
     hired_date = models.DateField(null=True, blank=True)
-    hire_price = models.DecimalField(max_digits=10, decimal_places=2)
+    return_date = models.DateField(null=True, blank=True)
+    hire_price = models.DecimalField(max_digits=7, decimal_places=2, help_text='单位：元/天')
 
     def __str__(self):
         return f"{self.brand} {self.model} - {self.category}"
